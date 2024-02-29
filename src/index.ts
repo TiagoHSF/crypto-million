62362da8609b84a0b4b9f49c6ebd92e7669c805b
TiagoSilvaTI <tiago.ferreira@globalfinanceiro.com.br>
2024-02-28 22:02:37 -0300

    ajustes projeto

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,11 @@
 import TelegramBot, { CallbackQuery, InlineKeyboardMarkup, ReplyKeyboardMarkup } from 'node-telegram-bot-api';
 import { Client, MessageMedia } from 'whatsapp-web.js';
-import { environmentDev, environmentProd } from './environment';
-import * as functions from 'firebase-functions';
+import { environmentProd } from './environment';
 
 //bot para tratar as mensagens
 const bot = new TelegramBot(environmentProd.botId, { polling: true });
 const groupId = environmentProd.telegramGroupId;
 const whatsappGroup = environmentProd.whatsappGroupId;
-
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCVJMwwYZFFFHk0AMa3fTKySqrn5loCVLg",
-    authDomain: "cryptomillion-208d3.firebaseapp.com",
-    projectId: "cryptomillion-208d3",
-    storageBucket: "cryptomillion-208d3.appspot.com",
-    messagingSenderId: "635527319270",
-    appId: "1:635527319270:web:af735fe575b1bb8006b527"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export const telegramBot = functions.https.onRequest((req: any, res: any) => {
-    // Verifica se é uma solicitação POST e se tem um corpo válido
-    if (req.method === 'POST' && req.body) {
-        // Processa a atualização do Telegram recebida no corpo da solicitação
-        bot.processUpdate(req.body);
-        res.status(200).send('OK');
-    } else {
-        // Retorna um erro se a solicitação não for POST ou se o corpo não for válido
-        res.status(400).send('Bad Request');
-    }
-});
 
 
 const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
