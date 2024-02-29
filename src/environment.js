f755836fd015315d8f1e46f4db19cc75051db522
TiagoSilvaTI <tiago.ferreira@globalfinanceiro.com.br>
2024-02-28 22:32:19 -0300

    fixes

---
+++ src/environment.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.environmentProd = exports.environmentDev = void 0;
+exports.environmentDev = {
+    botId: '7140994829:AAF5ZcuquH7p8jDVRqBaH9jCENqvvBbVdmA',
+    telegramGroupId: '-4130332581',
+    whatsappGroupId: ''
+};
+exports.environmentProd = {
+    botId: '6344531985:AAF38bqx62oUtSGKm2wFMi5JJS7WXj-LdNQ',
+    telegramGroupId: '-1002123923363',
+    whatsappGroupId: ''
+};
---
+++ src/index.js
@@ -0,0 +1,89 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_telegram_bot_api_1 = __importDefault(require("node-telegram-bot-api"));
+// import { Client, MessageMedia } from 'whatsapp-web.js';
+const environment_1 = require("./environment");
+//bot para tratar as mensagens
+const bot = new node_telegram_bot_api_1.default(environment_1.environmentProd.botId, { polling: true });
+const groupId = environment_1.environmentProd.telegramGroupId;
+// const whatsappGroup = environmentProd.whatsappGroupId;
+const urlRegex = /(\b(?!t\.me\/)(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+const mentionRegex = /@[\w.-]+/g;
+console.log('O bot está sendo iniciado... ');
+bot.on('message', (msg) => {
+    if (msg && msg.text) {
+        console.log('Mensagem recebida!');
+        let textMessage = msg.text || "";
+        textMessage = textMessage.replace(urlRegex, '');
+        textMessage = textMessage.replace(mentionRegex, '');
+        sendMessageToTelegramGroup(textMessage);
+    }
+});
+function sendMessageToTelegramGroup(msg) {
+    bot.sendMessage(groupId, msg)
+        .then(() => {
+        console.log('Mensagem enviada com sucesso!');
+    })
+        .catch((error) => {
+        console.error('Erro ao enviar mensagem:', error);
+    });
+}
+// function sendMessageToWhatsappGroup(msg: string) {
+//     console.log(client);
+//     client.on('ready', async () => {
+//         const groupName = 'CONTAS DO MÊS';
+//         try {
+//             const chats = await client.getChats();
+//             console.log(chats);
+//             const group = chats.find(chat => chat.isGroup && chat.name === groupName);
+//             if (group) {
+//                 // Envie a mensagem para o grupo
+//                 await group.sendMessage('Sua mensagem aqui');
+//                 console.log('Mensagem enviada com sucesso para o grupo!');
+//             } else {
+//                 console.error(`Grupo '${groupName}' não encontrado.`);
+//             }
+//         } catch (error) {
+//             console.error('Erro ao enviar mensagem para o grupo:', error);
+//         }
+//     });
+// }
+// client.initialize();
+// bot.on('polling_error', (error: any) => {
+//     exec('cd src && npx ts-node index.ts', (err: any, stdout: any, stderr: any) => {
+//         if (err) {
+//             console.error(`Erro ao recompilar o projeto: ${err}`);
+//             return;
+//         }
+//         console.log(`Projeto recompilado com sucesso!.`);
+//     });
+//     console.error(error);
+// });
+console.log('O bot está em execução...');
+setInterval(() => {
+    console.log("is alive...");
+}, 30000);
+// const puppeteer = require('puppeteer');
+// (async () => {function initializeWhatsApp(){
+//     const browser = await puppeteer.launch({ headless: false }); // Abre o navegador com a interface gráfica visível
+//     const page = await browser.newPage();
+//     await page.goto('https://web.whatsapp.com');
+//     // Espere até que o QR Code de login esteja disponível
+//     await page.waitForSelector('canvas');
+//     // Agora o usuário deve escanear o QR Code manualmente
+//     // Aqui você poderia inserir uma pausa ou um código para esperar que o usuário escaneie o QR Code
+//     // Agora, espera até que a mensagem de chat esteja disponível
+//     await page.waitForSelector('div[contenteditable="true"]');
+// }})
+// (async () => {
+//   // Agora o elemento está disponível, então você pode digitar uma mensagem
+//   await page.type('div[contenteditable="true"]', 'SIGNALS!');
+//   await page.keyboard.press('Enter');
+//   await page.type('div[contenteditable="true"]', 'Sua mensagem aqui');
+//   await page.keyboard.press('Enter');
+//   // Encerra o navegador após a execução do script
+//   // await browser.close();
+// })();
